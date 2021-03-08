a260a8cfa83ec1a81fc3ddebf3da2e5f42b1c536
8bitjustin <j.olson.digital@gmail.com>
2021-03-08 10:45:58 -0700

    Added prices variable to create an obj list of the prices. Added fulldesc dictionary created from the titles and prices lists being zipped. Reformed for list to add price of each beer next to it.

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ print('\nSearching for requested brew...\n')
 time.sleep(8)
 
 titles = driver.find_elements_by_css_selector('div.name.bold')
+prices = driver.find_elements_by_css_selector('span.price')
+
+fulldesc = dict(zip(titles, prices))
 
 print('\nRequested items found!\n')
 
@@ -46,8 +49,9 @@ EMAIL_ADDRESS = config.email
 EMAIL_PASS = config.pw
 
 body = f"Your results for: {search}\n\n"
-for title in titles:
-	body += '\t' + title.text + '\n\n'
+
+for t, p in fulldesc.items():
+	body += '\t' + t.text + ' / $' + p.text + '\n\n'
 
 msg = EmailMessage()
 msg['Subject'] = f'Tap and Bottle Left Hand Brewery Choices for {now}'
