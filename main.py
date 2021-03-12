3d04a107c3ecac26a3e62163967bf830907aeebe
8bitjustin <j.olson.digital@gmail.com>
2021-03-11 21:14:38 -0700

    Huge changes. In an attempt to make searching for multiple items easier, many changes were made. New search.py file created. This is imported into the main.py file. The purpose, is so multiple searches can be performed if requested. The search.py file runs a for loop for each argument input into the search function. This is ran within the main.py file, currently on line 31.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from datetime import datetime
 from email.message import EmailMessage
+from search import search
 import smtplib
 import config
 import time
@@ -13,33 +14,6 @@ op = webdriver.ChromeOptions()
 op.add_argument('headless')
 driver = webdriver.Chrome(options=op)
 
-driver.get("https://thetapandbottle.com/northstore")
-
-print("\nAccessing website...\n")
-
-time.sleep(5)
-
-search = 'left hand'
-
-frame = driver.find_elements_by_tag_name('iframe')[0]
-driver.switch_to.frame(frame)
-driver.find_element_by_class_name('search-label').click()
-
-driver.find_element_by_id("search").send_keys(search)
-
-print('\nSearching for requested brew...\n')
-
-time.sleep(8)
-
-titles = driver.find_elements_by_css_selector('div.name.bold')
-prices = driver.find_elements_by_css_selector('span.price')
-
-fulldesc = dict(zip(titles, prices))
-
-print('\nRequested items found!\n')
-
-print('\nPutting email together...\n')
-
 """EMAIL SECTION"""
 
 timestamp = datetime.now()
@@ -48,16 +22,15 @@ now = timestamp.strftime("%m/%d/%Y")
 EMAIL_ADDRESS = config.email
 EMAIL_PASS = config.pw
 
-body = f"Your results for: {search}\n\n"
-
-for t, p in fulldesc.items():
-	body += '\t' + t.text + ' / $' + p.text + '\n\n'
-
 msg = EmailMessage()
 msg['Subject'] = f'Tap and Bottle Left Hand Brewery Choices for {now}'
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = 'j.olson.digital@gmail.com'
-msg.set_content(body)
+
+# Add search keywords here
+msg.set_content(search('left hand', 'stone'))
+
+print('\nPutting email together...\n')
 
 smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 smtp.login(EMAIL_ADDRESS, EMAIL_PASS)
