9f31ccaf019d7d65b8d0b34972e7ecd0f41855b1
8bitjustin <j.olson.digital@gmail.com>
2021-03-12 11:01:42 -0700

    With last update, run box wouldn't close. This was most likely due to the driver being created within the function. To resolve, the search function was moved to the main.py file, and the search.py file was deleted due to no longer being used. Testing confirmed this resolved issue.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from datetime import datetime
 from email.message import EmailMessage
-from search import search
 import smtplib
 import config
 import time
@@ -13,6 +12,47 @@ print('\nRunning beer grabber...\n')
 op = webdriver.ChromeOptions()
 op.add_argument('headless')
 driver = webdriver.Chrome(options=op)
+
+"""SEARCH FUNCTION SECTION"""
+
+
+def search(*args):
+
+	body = ''
+
+	for arg in args:
+
+		driver.get("https://thetapandbottle.com/northstore")
+
+		print("\nAccessing website...\n")
+
+		time.sleep(5)
+
+		frame = driver.find_elements_by_tag_name('iframe')[0]
+
+		driver.switch_to.frame(frame)
+		driver.find_element_by_class_name('search-label').click()
+
+		driver.find_element_by_id("search").send_keys(arg)
+
+		print(f'\nSearching for requested brew... {arg.title()}\n')
+
+		time.sleep(8)
+
+		titles = driver.find_elements_by_css_selector('div.name.bold')
+		prices = driver.find_elements_by_css_selector('span.price')
+
+		print(f'\nLocated {len(titles)} items for {arg.title()}\n')
+
+		fulldesc = dict(zip(titles, prices))
+
+		body += f"You searched for: {arg.title()}\n\n"
+
+		for t, p in fulldesc.items():
+			body += '\t' + t.text + ' / $' + p.text + '\n\n'
+
+	return body
+
 
 """EMAIL SECTION"""
 
@@ -44,5 +84,4 @@ print('\nEmail sent!\n')
 print('\nClosing!\n')
 
 # Closes python box after program finishes
-driver.close()
 driver.quit()
